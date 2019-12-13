192771abb2e11a5b3f944bcd7c862c7da593bc17
Mr-Personne <h.sacha07@gmail.com>
2019-12-13 16:10:42 +0000

    tried making anti sql injection code

--- traitement.php
+++ traitement.php
@@ -1,19 +1,39 @@
 <?php
-    print_r($_POST);
+    // print_r($_POST);
     $search = $_POST['searchInput'];
 
     try{
         $db = new PDO('mysql:host=localhost;dbname=contacts;charset=utf8', 'root', '1234');
 
-        $answer = $db->query('SELECT * FROM contact WHERE first_name LIKE "'.$search.'%" OR first_name LIKE "%'.$search.'" OR first_name LIKE "%'.$search.'%"');
-        $data = $answer->fetch();
+        //------méthode qui marche mais moins sécurisé contre injection SQL------
+        // $answer = $db->query('SELECT * FROM contact WHERE first_name LIKE "'.$search.'%" OR first_name LIKE "%'.$search.'" OR first_name LIKE "%'.$search.'%"');
+        // $data = $answer->fetch();
+        // // $allData = $answer->fetchAll();
 
-        while($data){
-            echo '<p>'.$data['first_name'].' est de '.$data['country_code'].'</p>';
-            $data = $answer->fetch();
-        }
+        // //send allData to back to ajax so javascript can deal with generating message
+        // //because php wasnt really made to create front end stuff apparently?
+        // while($data){
+        //     echo '<p>'.$data['first_name'].' est de '.$data['country_code'].'</p>';
+        //     $data = $answer->fetch();
+        // }
 
-        $answer->closeCursor();
+        // $answer->closeCursor();
+
+        //------méthode plus sécurisé contre injection SQL------
+        $answer = $db->prepare('SELECT * FROM contact WHERE first_name LIKE :searchStart OR first_name LIKE :searchMid OR first_name LIKE :searchEnd');
+
+        //cette partie permet de faire fonctionner la recherche sql 'LIKE'
+        $searchStart = $search."%";
+        $searchMid = "%".$search."%";
+        $searchEnd = "%".$search;
+        $answer->bindParam(':searchStart', $searchStart, PDO::PARAM_STR);
+        $answer->bindParam(':searchMid', $searchMid, PDO::PARAM_STR);
+        $answer->bindParam(':searchEnd', $searchEnd, PDO::PARAM_STR);
+
+        $answer->execute();
+        $allData = $answer->fetchAll();
+        echo json_encode($allData);
+        // print_r($allData);
 
 
     } catch (Exception $e) {
