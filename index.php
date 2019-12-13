03b1cef3b4803937db2d7695c3347b01d86e1d8c
Mr-Personne <h.sacha07@gmail.com>
2019-12-13 10:32:22 +0000

    mise en place de base

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+    // try{
+    //     $db = new PDO('mysql:host=localhost;dbname=contacts;charset=utf8', 'root', '1234');
+
+    //     $answer = $db->query('SELECT * FROM contact WHERE first_name LIKE "A%" OR first_name LIKE "%A" OR first_name LIKE "%A%"');
+    //     $data = $answer->fetch();
+
+    //     while($data){
+    //         echo '<p>'.$data['first_name'].' est de '.$data['country_code'].'</p>';
+    //         $data = $answer->fetch();
+    //     }
+
+    //     $answer->closeCursor();
+
+
+    // } catch (Exception $e) {
+
+    //     //verif si erreur et met un message contenu dans $e qui dévoilera moins que le message erreur de base
+    //     die('Erreur : ' . $e->getMessage());
+    
+    // }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +28,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link href= style.css rel= stylesheet>
 </head>
 <body>
     <h1><span>&#161;</span>Hola! El gooG!</h1>
@@ -12,5 +37,7 @@
         <input type="text" name="searchInput">
         <button type="submit" id="submit"><span>&#161;</span>El gooG it!</button>
     </form>
+
+    <script src="script.js"></script>
 </body>
 </html>
---
+++ traitement.php
@@ -0,0 +1,22 @@
+<?php
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=contacts;charset=utf8', 'root', '1234');
+
+        $answer = $db->query('SELECT * FROM contact WHERE first_name LIKE "A%" OR first_name LIKE "%A" OR first_name LIKE "%A%"');
+        $data = $answer->fetch();
+
+        while($data){
+            echo '<p>'.$data['first_name'].' est de '.$data['country_code'].'</p>';
+            $data = $answer->fetch();
+        }
+
+        $answer->closeCursor();
+
+
+    } catch (Exception $e) {
+
+        //verif si erreur et met un message contenu dans $e qui dévoilera moins que le message erreur de base
+        die('Erreur : ' . $e->getMessage());
+    
+    }
+?>
