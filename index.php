9c1132241014bb8b67ae41de73d33e01876b0633
Mr-Personne <h.sacha07@gmail.com>
2019-12-13 10:10:15 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><span>&#161;</span>Hola! El gooG!</h1>
+    <form action="#">
+        <input type="text" name="searchInput">
+        <button type="submit" id="submit"><span>&#161;</span>El gooG it!</button>
+    </form>
+</body>
+</html>
