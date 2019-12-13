e613c6c60b2685016d78d863d8b25b1ce8d0c2f2
Mr-Personne <h.sacha07@gmail.com>
2019-12-13 11:20:21 +0000

    created ajax call asynch function to traitement.php

---
+++ script.js
@@ -0,0 +1,43 @@
+const myForm = document.querySelector('form');
+const inputSearch = document.querySelector('input[name="searchInput"]');
+
+function ajaxCallAsynch(){
+    console.dir(myForm);
+    console.log(myForm);
+    if (inputSearch.value.length === 0){
+        alert('Please enter something before El Goog-ing it');
+        return;
+    }
+    else{
+        var formData = new FormData(myForm);
+
+        var paramAjax = {
+            method : "POST",
+            body : formData
+        };
+
+        fetch('traitement.php', paramAjax).then(function(response){
+
+            console.dir(response);
+            return response.text();
+
+        }).then(function(data){
+
+            var resultDiv = document.querySelector('#search-result');
+            resultDiv.innerHTML = data;
+        });
+        console.log("lol");
+    }
+};
+
+myForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    ajaxCallAsynch();
+
+    
+});
+
+inputSearch.addEventListener("keyup", function(event){
+    console.log(event.target.value);
+});
